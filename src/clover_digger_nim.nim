162e6bf177bb036b67f6a0e3efadd725f291d7bb
Gary Glover <dev@thebigg69.co.uk>
2024-01-26 16:33:55 +0000

    MCP23S17 Flash LED

--- src/clover_digger_nim.nim
+++ src/clover_digger_nim.nim
@@ -1,21 +1,68 @@
 import picostdlib
+import picostdlib/hardware/spi
 
 stdioInitAll()
 
-setupGpio(col, 10, Out)
-col.put(High)
+type
+  RegisterAddress {.size: sizeof(uint8).} = enum
+    IoDirA
+    IoDirB
+    IPolA
+    IPolB
+    GpintentA
+    GpintentB
+    DefValA
+    DefValB
+    IntConA
+    IntConB
+    Iocon
+    IoCon2
+    GppuA
+    GppuB
+    IntfA
+    IntfB
+    IntcapA
+    IntcapB
+    GpioA
+    GpioB
+    OlatA
+    OlatB
 
-setupGpio(row, 8, In)
-row.pullDown
+echo "Init SPI"
+discard spi0.init(10000000)
+spi0.setFormat(8, Pol0, Phase0, MsbFirst)
+DefaultSpiSckPin.setFunction(Spi)
+DefaultSpiTxPin.setFunction(Spi)
+DefaultSpiRxPin.setFunction(Spi)
 
-sleepMs(250)
+echo "Init CS"
+DefaultSpiCsnPin.setFunction(Spi)
+DefaultSpiCsnPin.init
+DefaultSpiCsnPin.setDir(Out)
+DefaultSpiCsnPin.put(High)
 
-var state = row.get
-var readState = state
+echo "Wait for SPI Writable"
+while not spi0.isWritable:
+  discard
+
+echo "Set Iocon"
+DefaultSpiCsnPin.put(Low)
+discard spi0.writeBlocking(0x40u8, ord(Iocon), 0x0u8)
+DefaultSpiCsnPin.put(High)
+
+echo "Set GPA to out"
+DefaultSpiCsnPin.put(Low)
+discard spi0.writeBlocking(0x40u8, ord(IoDirA), 0x0u8)
+DefaultSpiCsnPin.put(High)
 
 while true:
-  readState = row.get
-  if state != readState:
-     state = readState
-     echo "Button changed to" & $readState
-  sleepMs(250)
+  echo "Set GPA Low"
+  DefaultSpiCsnPin.put(Low)
+  discard spi0.writeBlocking(0x40u8, ord(OlatA), 0x0u8)
+  DefaultSpiCsnPin.put(High)
+  sleepMs(500)
+  echo "Set GPA High"
+  DefaultSpiCsnPin.put(Low)
+  discard spi0.writeBlocking(0x40u8, ord(OlatA), 0xffu8)
+  DefaultSpiCsnPin.put(High)
+  sleepMs(500)
