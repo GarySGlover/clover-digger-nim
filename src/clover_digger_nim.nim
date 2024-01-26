d3b5ed005f8a92fd065028b69e440637d3ec8c75
Gary Glover <dev@thebigg69.co.uk>
2024-01-26 17:55:58 +0000

    Read button from MCP23S17

--- src/clover_digger_nim.nim
+++ src/clover_digger_nim.nim
@@ -3,6 +3,10 @@ import picostdlib/hardware/spi
 
 stdioInitAll()
 
+echo "Setting col to out"
+setupGpio(col, 8, Out)
+col.put(Low)
+
 type
   RegisterAddress {.size: sizeof(uint8).} = enum
     IoDirA
@@ -50,19 +54,32 @@ DefaultSpiCsnPin.put(Low)
 discard spi0.writeBlocking(0x40u8, ord(Iocon), 0x0u8)
 DefaultSpiCsnPin.put(High)
 
-echo "Set GPA to out"
+echo "Set GPA to in"
 DefaultSpiCsnPin.put(Low)
-discard spi0.writeBlocking(0x40u8, ord(IoDirA), 0x0u8)
+discard spi0.writeBlocking(0x40u8, ord(IoDirA), 0xffu8)
 DefaultSpiCsnPin.put(High)
 
+echo "Set GPA to pull-up"
+DefaultSpiCsnPin.put(Low)
+discard spi0.writeBlocking(0x40u8, ord(GppuA), 0xffu8)
+DefaultSpiCsnPin.put(High)
+
+echo "Getting initial state"
+var readState: uint8
+DefaultSpiCsnPin.put(Low)
+discard spi0.writeBlocking(0x41u8, ord(GpioA))
+discard spi0.readBlocking(0, readState.addr, 1)
+DefaultSpiCsnPin.put(High)
+var state = readState
+
+echo "Initial state is" & $readState
+
 while true:
-  echo "Set GPA Low"
   DefaultSpiCsnPin.put(Low)
-  discard spi0.writeBlocking(0x40u8, ord(OlatA), 0x0u8)
+  discard spi0.writeBlocking(0x41u8, ord(GpioA))
+  discard spi0.readBlocking(0, readState.addr, 1)
   DefaultSpiCsnPin.put(High)
-  sleepMs(500)
-  echo "Set GPA High"
-  DefaultSpiCsnPin.put(Low)
-  discard spi0.writeBlocking(0x40u8, ord(OlatA), 0xffu8)
-  DefaultSpiCsnPin.put(High)
-  sleepMs(500)
+  if state != readState:
+     state = readState
+     echo "Button changed to" & $readState
+  sleepMs(2)
