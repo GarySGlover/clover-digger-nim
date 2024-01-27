0cd71138cf019f5a61193d599b9bea32e5af9788
Gary Glover <dev@thebigg69.co.uk>
2024-01-27 21:40:05 +0000

    First switch working

--- src/clover_digger_nim.nim
+++ src/clover_digger_nim.nim
@@ -1,11 +1,20 @@
 import picostdlib
 import picostdlib/hardware/spi
+import std/bitops
 
 stdioInitAll()
 
-echo "Setting col to out"
-setupGpio(col, 8, Out)
-col.put(Low)
+echo "Setting rows to in"
+setupGpio(up, 2, In)
+up.pullUp
+setupGpio(right, 3, In)
+right.pullUp
+setupGpio(down, 6, In)
+down.pullUp
+setupGpio(left, 7, In)
+left.pullUp
+setupGpio(push, 8, In)
+push.pullUp
 
 type
   RegisterAddress {.size: sizeof(uint8).} = enum
@@ -54,32 +63,50 @@ DefaultSpiCsnPin.put(Low)
 discard spi0.writeBlocking(0x40u8, ord(Iocon), 0x0u8)
 DefaultSpiCsnPin.put(High)
 
-echo "Set GPA to in"
+echo "Set GPx to out"
 DefaultSpiCsnPin.put(Low)
-discard spi0.writeBlocking(0x40u8, ord(IoDirA), 0xffu8)
+discard spi0.writeBlocking(0x40u8, ord(IoDirA), 0x00u8)
+discard spi0.writeBlocking(0x40u8, ord(IoDirB), 0x00u8)
 DefaultSpiCsnPin.put(High)
 
-echo "Set GPA to pull-up"
+echo "Set GPx to high"
 DefaultSpiCsnPin.put(Low)
-discard spi0.writeBlocking(0x40u8, ord(GppuA), 0xffu8)
+discard spi0.writeBlocking(0x40u8, ord(OlatA), 0xffu8)
+discard spi0.writeBlocking(0x40u8, ord(OlatB), bitxor(0xffu8, 1u8))
 DefaultSpiCsnPin.put(High)
 
 echo "Getting initial state"
-var readState: uint8
-DefaultSpiCsnPin.put(Low)
-discard spi0.writeBlocking(0x41u8, ord(GpioA))
-discard spi0.readBlocking(0, readState.addr, 1)
-DefaultSpiCsnPin.put(High)
-var state = readState
+var upState = up.get
+var rightState = right.get
+var downState = down.get
+var leftState = left.get
+var pushState = push.get
 
-echo "Initial state is" & $readState
+echo "Initial up state is" & $upState
+echo "Initial right state is" & $rightState
+echo "Initial downn state is" & $downState
+echo "Initial left state is" & $rightState
+echo "Initial push state is" & $leftState
 
 while true:
-  DefaultSpiCsnPin.put(Low)
-  discard spi0.writeBlocking(0x41u8, ord(GpioA))
-  discard spi0.readBlocking(0, readState.addr, 1)
-  DefaultSpiCsnPin.put(High)
-  if state != readState:
-     state = readState
-     echo "Button changed to" & $readState
+  var upStateRead = up.get
+  var rightStateRead = right.get
+  var downStateRead = down.get
+  var leftStateRead = left.get
+  var pushStateRead = push.get
+  if upState != upStateRead:
+     upState = upStateRead
+     echo "Up button changed to" & $upState
+  if rightState != rightStateRead:
+     rightState = rightStateRead
+     echo "Right button changed to" & $rightState
+  if downState != downStateRead:
+     downState = downStateRead
+     echo "Down button changed to" & $downState
+  if leftState != leftStateRead:
+     leftState = leftStateRead
+     echo "Left button changed to" & $leftState
+  if pushState != pushStateRead:
+     pushState = pushStateRead
+     echo "Push button changed to" & $pushState
   sleepMs(2)
